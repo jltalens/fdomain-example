93446257bb97c88cbeebed277e60787484f429dd
jltalens <jltalens@hotmail.com>
2018-04-01 20:05:00 +0100

    Half way through profile summary client

--- src/main/scala/org.jtalens.fdomain/model/ProfileSummary.scala
+++ src/main/scala/org.jtalens.fdomain/model/ProfileSummary.scala
@@ -1,3 +1,11 @@
 package org.jtalens.fdomain.model
 
+import io.circe.{ Decoder, Encoder }
+import io.circe.generic.semiauto.{ deriveDecoder, deriveEncoder }
+
 case class ProfileSummary(id: String, allowsCommunications: Boolean, score: Long)
+
+object ProfileSummary {
+  implicit val encodeAccountSummary: Encoder[ProfileSummary] = deriveEncoder[ProfileSummary]
+  implicit val decodeAccountSummary: Decoder[ProfileSummary] = deriveDecoder[ProfileSummary]
+}
--- src/main/scala/org.jtalens.fdomain/repository/interpreter/ProfileSummaryIntIO.scala
+++ src/main/scala/org.jtalens.fdomain/repository/interpreter/ProfileSummaryIntIO.scala
@@ -1,15 +1,47 @@
 package org.jtalens.fdomain.repository.interpreter
 
+import cats.data.Kleisli
 import cats.effect.IO
 import cats.~>
+import org.http4s.circe.jsonOf
+import org.http4s.client.Client
+import org.http4s.{ EntityDecoder, Status, Uri }
+import org.jtalens.fdomain.model.ProfileSummary
 import org.jtalens.fdomain.repository.{ Fetch, ProfileSummaryAlgF }
 
 trait InterpreterB[F[_]] {
-  def compiler: ProfileSummaryAlgF ~> F
+  def compiler(profileSummaryService: ProfileSummaryService, app: AppConfig): ProfileSummaryAlgF ~> F
 }
 
-class ProfileSummaryIntIO extends InterpreterB[IO] {
-  def compiler: ~>[ProfileSummaryAlgF, IO] = new (ProfileSummaryAlgF ~> IO) {
-    def apply[A](fa: ProfileSummaryAlgF[A]): IO[A] = ???
+object ProfileSummaryIntIO extends InterpreterB[IO] {
+  def compiler(profileSummaryService: ProfileSummaryService, app: AppConfig): ~>[ProfileSummaryAlgF, IO] = new (ProfileSummaryAlgF ~> IO) {
+    def apply[A](fa: ProfileSummaryAlgF[A]): IO[A] = fa match {
+      case Fetch(id) => profileSummaryService.fetch(id).run(app)
+    }
   }
+}
+
+class ProfileSummaryService(client: Client[IO]) {
+
+  import ProfileSummaryService._
+
+  def fetch(id: String): Kleisli[IO, AppConfig, Either[String, ProfileSummary]] = Kleisli { config =>
+    val target = Uri.fromString(config.profileConfig.getProfileUri).right.get / id
+    client.get(target) {
+      case Status.Successful(r) => r.attemptAs[ProfileSummary].leftMap(_.message).value
+      case r => r.as[String]
+        .map(b => Left(s"Request $target failed with status ${r.status.code} and body $b"))
+    }
+  }
+
+}
+
+case class AppConfig(profileConfig: ProfileConfig)
+
+trait ProfileConfig {
+  val getProfileUri: String
+}
+
+object ProfileSummaryService {
+  implicit def accountSummaryDecoder: EntityDecoder[IO, ProfileSummary] = jsonOf[IO, ProfileSummary]
 }
